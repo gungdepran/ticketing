82af11ff63f379b52fb2cd3046303db44a125b20
gnendraaw <itsgnendraaw@gmail.com>
2023-01-19 08:11:44 +0800

    setup config & database

---
+++ app/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE_URL', '/php-transportation-ticketing');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'ticketing');
