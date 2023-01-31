0dfd19a8586d1d6c068bca9dda8c4ecaf926ea95
gnendraaw <itsgnendraaw@gmail.com>
2023-01-31 22:53:49 +0800

    add admin dashboard page

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,3 +1,21 @@
 <div class="container-fluid">
-    <?php foreach($_SESSION['user'] as $key => $user) echo $key . ' ' ?>
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card shadow border-left-primary">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs text-primary font-weight-bold text-uppercase mb-1">
+                                Jumlah Petugas
+                            </div>
+                            <h5 class="mb-0 font-weight-bold text-gray-800">14</h5>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-user fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
